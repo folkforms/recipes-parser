c209972341e8ad787ddc9f329f7bed2c48222d2a
folkforms <alan.bergin@gmail.com>
2021-12-21 22:50:57 +0000

    Make meta data parser more lenient

--- meta-data-parser/metaDataParser.js
+++ meta-data-parser/metaDataParser.js
@@ -3,10 +3,20 @@ const parseMetaData = obj => {
 
   obj.metaData = {};
   obj.metaDataUnparsed.forEach(line => {
-    if(line.startsWith("FROM: ")) {
+    if(line.length === 0 || line.startsWith("//")) {
+      return;
+    } else if(line.toLowerCase().startsWith("from: ")) {
       obj.metaData.from = line.substring(6);
+    } else if(line.toLowerCase().startsWith("from ")) {
+      obj.metaData.from = line.substring(5); // FIXME Should I warn here and suggest using a proper metadata tag "FROM:"?
+    } else if(line.startsWith("https://")) {
+      obj.metaData.from = line;
+    } else if(line.toLowerCase().startsWith("source: ")) {
+      obj.metaData.from = line.substring(8); // FIXME Should I warn about using "SOURCE" here and suggest that it should be replaced with "FROM"?
     } else if(line.startsWith("SERVES: ")) {
       obj.metaData.serves = line.substring(8); // FIXME Convert into number
+    } else if(line.startsWith("Serves ")) {
+      obj.metaData.serves = line.substring(7); // FIXME Convert into number
     } else if(line.startsWith("TIME: ")) {
       obj.metaData.time = line.substring(6); // FIXME Parse time into raw minutes
     } else if(line.startsWith("TAGS: ")) {
@@ -14,7 +24,8 @@ const parseMetaData = obj => {
     } else if(line.startsWith("SHOPPING LIST: ")) {
       obj.metaData.shoppingList = line.substring(15).split(", ");
     } else {
-      console.log(`Unknown metadata item: '${line}' in file '${obj.filename}'`);
+      // Do nothing. Probably just some descriptive text.
+      // console.log(`Unknown metadata item: '${line}' in file '${obj.filename}'`);
     }
   });
   delete obj.metaDataUnparsed;
