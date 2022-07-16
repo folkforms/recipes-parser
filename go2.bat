9181d8e9982d0ce942079bb33cd20453d3bb00b4
folkforms <alan.bergin@gmail.com>
2022-07-16 13:34:38 +0100

    Add batch files

--- go2.bat
+++ go2.bat
@@ -1,3 +1,6 @@
 @echo off
-call yarn cli --file input.txt --output ./recipes-full.json %*
+
+call C:\Users\Alan\AppData\Roaming\npm\yarn.cmd cli --file input.txt --output ./recipes-full.json %*
+if %errorlevel% neq 0 exit /b %errorlevel%
 copy /Y recipes-full.json ..\recipes-react-app\src\recipes-full.json
+if %errorlevel% neq 0 exit /b %errorlevel%
