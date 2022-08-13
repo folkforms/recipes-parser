72200294cb0987c948d40c26c4d5a4a702de986a
folkforms <alan.bergin@gmail.com>
2022-08-13 07:56:06 +0100

    Add specific units to foods.json for items like '5 cloves garlic'

--- ingredients-parser/ingredientsParser.js
+++ ingredients-parser/ingredientsParser.js
@@ -47,7 +47,7 @@ const parseIngredients = obj => {
         }
 
         // Type
-        const regexpUnit = new RegExp("^(\\d+|\\d\/\\d|\\d\\.\\d+)\\s?(kg|g|tsp|dsp|rounded dsp|Tbsp|tbsp|tablespoons|ml|small tin|tin|slices)");
+        const regexpUnit = new RegExp("^(\\d+|\\d\/\\d|\\d\\.\\d+)\\s?(kg|g|tsp|dsp|rounded dsp|Tbsp|tbsp|tablespoons|ml|small tin|tin|clove|slices)");
         const matchUnit = regexpUnit.exec(item);
         if(matchUnit && matchUnit.length > 2) {
           hasUnit = true;
@@ -63,46 +63,71 @@ const parseIngredients = obj => {
           parsedIngredient.count = matchFoo[1] / matchFoo[2];
         }
 
+        // FIXME
+        // Move this to a file
+        const genericConversions = [
+          { "unit": "g", "multiplier": 1 },
+          { "unit": "kg", "multiplier": 1000 },
+          { "unit": "ml", "multiplier": 1 },
+          { "unit": "small tin", "multiplier": 200 },
+          { "unit": "tin", "multiplier": 400 },
+          { "unit": "tsp", "multiplier": 5 },
+          { "unit": "dsp", "multiplier": 10 },
+          { "unit": "Tbsp", "multiplier": 15 },
+          { "unit": "tbsp", "multiplier": 15 },
+          { "unit": "tablespoon", "multiplier": 15 }
+        ];
+        const genericUnits = genericConversions.map(c => c.unit);
+
         // Convert count and type from natural language "0.5 tsp" to standard term "2.5 g"
         if(parsedIngredient.count) {
+          const data = foodUtils.getData(parsedIngredient);
+          // console.log(`#### data = ${JSON.stringify(data)}`);
           if(parsedIngredient.unit === null) {
-            const data = foodUtils.getData(parsedIngredient);
             if(data && data.weightOfOneItem) {
               parsedIngredient.unit = "g";
               parsedIngredient.count = String(parsedIngredient.count * data.weightOfOneItem);
             } else {
-              throw new Error(`(1) Could not parse count = ${parsedIngredient.count} and unit = ${parsedIngredient.unit} for ${JSON.stringify(parsedIngredient)}`);
+              throw new Error(`Conversion error (1): No unit and no weightOfOneItem for ${JSON.stringify(parsedIngredient)} in ${obj.filename}`);
             }
-          } else if(parsedIngredient.unit === "g") {
-            // Do nothing
-          } else if(parsedIngredient.unit === "kg") {
+          } else if(data.specificUnits && data.specificUnits.includes(parsedIngredient.unit) && data.weightOfOneItem) {
             parsedIngredient.unit = "g";
-            parsedIngredient.count = String(parsedIngredient.count * 1000);
-          } else if(parsedIngredient.unit === "ml") {
+            parsedIngredient.count = String(parsedIngredient.count * data.weightOfOneItem);
+            // console.log(`@@@@ parsedIngredient = ${JSON.stringify(parsedIngredient)}`);
+          } else if(genericUnits.includes(parsedIngredient.unit)) {
+            const multiplier = genericConversions.filter(c => c.unit === parsedIngredient.unit).multiplier;
             parsedIngredient.unit = "g";
-          } else if(parsedIngredient.unit === "small tin") {
-            parsedIngredient.unit = "g";
-            parsedIngredient.count = String(parsedIngredient.count * 200);
-          } else if(parsedIngredient.unit === "tin") {
-            parsedIngredient.unit = "g";
-            parsedIngredient.count = String(parsedIngredient.count * 400);
-          } else if(parsedIngredient.unit === "tsp") {
-            parsedIngredient.unit = "g";
-            parsedIngredient.count = String(parsedIngredient.count * 5);
-          } else if(parsedIngredient.unit === "dsp" || parsedIngredient.unit === "rounded dsp") {
-            parsedIngredient.unit = "g";
-            parsedIngredient.count = String(parsedIngredient.count * 10);
-          } else if(parsedIngredient.unit === "Tbsp" || parsedIngredient.unit === "tbsp" || parsedIngredient.unit === "tablespoons") {
-            parsedIngredient.unit = "g";
-            parsedIngredient.count = String(parsedIngredient.count * 15);
-          } else if(parsedIngredient.name === "bacon" && parsedIngredient.unit === "slices") {
-            parsedIngredient.unit = "g";
-            parsedIngredient.count = String(parsedIngredient.count * 40);
-          } else if(parsedIngredient.name === "parma ham" && parsedIngredient.unit === "slices") {
-            parsedIngredient.unit = "g";
-            parsedIngredient.count = String(parsedIngredient.count * 21);
+            parsedIngredient.count = String(parsedIngredient.count * multiplier);
+          // } else if(parsedIngredient.unit === "g") {
+          //   // Do nothing
+          // } else if(parsedIngredient.unit === "kg") {
+          //   parsedIngredient.unit = "g";
+          //   parsedIngredient.count = String(parsedIngredient.count * 1000);
+          // } else if(parsedIngredient.unit === "ml") {
+          //   parsedIngredient.unit = "g";
+          // } else if(parsedIngredient.unit === "small tin") {
+          //   parsedIngredient.unit = "g";
+          //   parsedIngredient.count = String(parsedIngredient.count * 200);
+          // } else if(parsedIngredient.unit === "tin") {
+          //   parsedIngredient.unit = "g";
+          //   parsedIngredient.count = String(parsedIngredient.count * 400);
+          // } else if(parsedIngredient.unit === "tsp") {
+          //   parsedIngredient.unit = "g";
+          //   parsedIngredient.count = String(parsedIngredient.count * 5);
+          // } else if(parsedIngredient.unit === "dsp" || parsedIngredient.unit === "rounded dsp") {
+          //   parsedIngredient.unit = "g";
+          //   parsedIngredient.count = String(parsedIngredient.count * 10);
+          // } else if(parsedIngredient.unit === "Tbsp" || parsedIngredient.unit === "tbsp" || parsedIngredient.unit === "tablespoons") {
+          //   parsedIngredient.unit = "g";
+          //   parsedIngredient.count = String(parsedIngredient.count * 15);
+          // } else if(parsedIngredient.name === "bacon" && parsedIngredient.unit === "slices") {
+          //   parsedIngredient.unit = "g";
+          //   parsedIngredient.count = String(parsedIngredient.count * 40);
+          // } else if(parsedIngredient.name === "parma ham" && parsedIngredient.unit === "slices") {
+          //   parsedIngredient.unit = "g";
+          //   parsedIngredient.count = String(parsedIngredient.count * 21);
           } else {
-            throw new Error(`(2) Could not parse count = ${parsedIngredient.count} and unit = ${parsedIngredient.unit} for ${JSON.stringify(item)}`);
+            throw new Error(`Conversion error (2): Unknown unit in ${JSON.stringify(parsedIngredient)} from ${JSON.stringify(item)} in ${obj.filename}`);
           }
         }
 
@@ -125,8 +150,8 @@ const parseIngredients = obj => {
       }
     }
   });
-  obj.calories = calculateCalories(obj.ingredientsParsed, obj);
   if(obj.metaData.serves) {
+    obj.calories = calculateCalories(obj.ingredientsParsed, obj);
     obj.caloriesPerServing = obj.calories / obj.metaData.serves;
   }
   return obj;
