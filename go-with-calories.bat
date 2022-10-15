502a33d78e6fcaf1de97462f357c7fca6a79546d
folkforms <alan.bergin@gmail.com>
2022-10-15 20:46:41 +0100

    Use --no-calories in go.bat

--- go-with-calories.bat
+++ go-with-calories.bat
@@ -1,6 +1,6 @@
 @echo off
 
-call C:\Users\Alan\AppData\Roaming\npm\yarn.cmd cli --input f:/documents/food/**/*.md --output ./recipes-full.json --no-calories %*
+call C:\Users\Alan\AppData\Roaming\npm\yarn.cmd cli --input f:/documents/food/**/*.md --output ./recipes-full.json %*
 if %errorlevel% neq 0 exit /b %errorlevel%
 copy /Y recipes-full.json ..\recipes-react-app\src\recipes-full.json
 if %errorlevel% neq 0 exit /b %errorlevel%
